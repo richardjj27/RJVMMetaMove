7b493cbb3d5093f258c8a48850149bbfe45a0443
richardjj27 <60007803+richardjj27@users.noreply.github.com>
2023-03-09 16:49:01 +0000

    Split core and meta data functions

--- export-vmmetadata.ps1
+++ export-vmmetadata.ps1
@@ -5,10 +5,10 @@
 #   2. Move
 #   3. Import (Set-VMMetadata) tags/attributes
 #   4. Check and give update
-# Encryption/disk policy
+# Encryption/disk policy (basically, if datastore is encrypted OR the specific VM is encrypted, its 'yes')
 # Split tag/attributes and the rest into separate functions.
 
-function Get-VMMetadata {
+function Get-VMMetaData {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -19,47 +19,25 @@ function Get-VMMetadata {
     $VM = Get-VM -Name $VMName
     $CustomAttrList = Get-CustomAttribute -Server $VM.Uid.Split(":")[0].Split("@")[1]
 
-    if ($VM) {
+    If ($VM) {
         # Get all custom attributes for the virtual machine
-        
+
         $output = @()
-        $vcserver = $VM.Uid.Split(":")[0].Split("@")[1]
+        #$vcserver = $VM.Uid.Split(":")[0].Split("@")[1]
 
         # Create an object to hold the custom attributes
         $CustomObject = New-Object -TypeName PSObject
-        $CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name # yes
-        $CustomObject | Add-Member -Name "AttributeKey" -MemberType NoteProperty -value $null
-        $CustomObject | Add-Member -Name "AttributeName" -MemberType NoteProperty -value $null
-        $CustomObject | Add-Member -Name "AttributeValue" -MemberType NoteProperty -value $null
-        $CustomObject | Add-Member -Name "Tag" -MemberType NoteProperty -value $null
-        $CustomObject | Add-Member -Name "DiskName" -MemberType NoteProperty -value $null
-        $CustomObject | Add-Member -Name "DiskLayout" -MemberType NoteProperty -value $null
-        $CustomObject | Add-Member -Name "DiskDatastore" -MemberType NoteProperty -value $null
-        #$CustomObject | Add-Member -Name "DiskDatastoreEncryptionStatus" -MemberType NoteProperty -value $null
-        $CustomObject | Add-Member -Name "DiskSizeGB" -MemberType NoteProperty -value $null
-        $CustomObject | Add-Member -Name "NetworkAdapter" -MemberType NoteProperty -value $null
+        #$CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name # yes
+        #$CustomObject | Add-Member -Name "AttributeKey" -MemberType NoteProperty -value $null
+        #$CustomObject | Add-Member -Name "AttributeName" -MemberType NoteProperty -value $null
+        #$CustomObject | Add-Member -Name "AttributeValue" -MemberType NoteProperty -value $null
+        #$CustomObject | Add-Member -Name "Tag" -MemberType NoteProperty -value $null
+        #$output += $CustomObject
 
-        # --singles
-        $CustomObject | Add-Member -Name "vCenter" -MemberType NoteProperty -value $vcserver # yes
-        $CustomObject | Add-Member -Name "Host" -MemberType NoteProperty -value (get-vmhost -vm $vm).name # yes
-        $CustomObject | Add-Member -Name "HostVersion" -MemberType NoteProperty -value (get-vmhost -vm $vm).version # yes
-        $CustomObject | Add-Member -Name "HostBuild" -MemberType NoteProperty -value (get-vmhost -vm $vm).build # yes
-        $CustomObject | Add-Member -Name "Datacenter" -MemberType NoteProperty -value (Get-Datacenter -Server $vcserver -vm $vm.name) # yes
-        $CustomObject | Add-Member -Name "Cluster" -MemberType NoteProperty -value (Get-Cluster -Server $vcserver -vm $vm.name) # yes
-        $CustomObject | Add-Member -Name "ResourcePool" -MemberType NoteProperty -value (Get-ResourcePool -Server $vcserver -VM $VM) # yes
-        $CustomObject | Add-Member -Name "MemoryGB" -MemberType NoteProperty -value $vm.memorygb # yes
-        $CustomObject | Add-Member -Name "CPU" -MemberType NoteProperty -value $vm.NumCpu # yes
-        $CustomObject | Add-Member -Name "ToolsVersion" -MemberType NoteProperty -value $vm.extensiondata.guest.toolsversion # yes
-        $CustomObject | Add-Member -Name "Notes" -MemberType NoteProperty -value $vm.notes # check
-        $CustomObject | Add-Member -Name "Powerstate" -MemberType NoteProperty -value $vm.powerstate # yes
-        $CustomObject | Add-Member -Name "GuestOS" -MemberType NoteProperty -value $vm.extensiondata.guest.guestfullname # yes
-        $CustomObject | Add-Member -Name "Snapshot" -MemberType NoteProperty -value ($vm | get-snapshot).created # yes
-        $output += $CustomObject
-
-        # -- multiples
+        # Multiples
         # Loop through each custom attribute and add it to the object
         $CustomAttributes = $VM.ExtensionData.CustomValue
-        foreach ($Attribute in $CustomAttributes) {
+        foreach ($attribute in $CustomAttributes) {
             if ($attribute.Value){
                 $CustomObject = New-Object -TypeName PSObject
                 $CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name
@@ -78,7 +56,59 @@ function Get-VMMetadata {
             $CustomObject | Add-Member -Name "Tag" -MemberType NoteProperty -value $Tag.Tag
             $output += $CustomObject
         }
+        return $output
+    }
+    else {
+        Write-Error "Virtual machine not found."
+    }
+}
 
+function Get-VMCoreData {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$VMName
+    )
+    
+    # Get the virtual machine
+    $VM = Get-VM -Name $VMName
+    #$CustomAttrList = Get-CustomAttribute -Server $VM.Uid.Split(":")[0].Split("@")[1]
+
+    if ($VM) {
+        # Get all custom attributes for the virtual machine
+        
+        $output = @()
+        $vcserver = $VM.Uid.Split(":")[0].Split("@")[1]
+
+        # Create an object to hold the custom attributes
+        $CustomObject = New-Object -TypeName PSObject
+
+        #$CustomObject | Add-Member -Name "DiskName" -MemberType NoteProperty -value $null
+        #$CustomObject | Add-Member -Name "DiskLayout" -MemberType NoteProperty -value $null
+        #$CustomObject | Add-Member -Name "DiskDatastore" -MemberType NoteProperty -value $null
+        ##$CustomObject | Add-Member -Name "DiskDatastoreEncryptionStatus" -MemberType NoteProperty -value $null
+        #$CustomObject | Add-Member -Name "DiskSizeGB" -MemberType NoteProperty -value $null
+        #$CustomObject | Add-Member -Name "NetworkAdapter" -MemberType NoteProperty -value $null
+
+        # --singles
+        $CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name # yes
+        $CustomObject | Add-Member -Name "vCenter" -MemberType NoteProperty -value $vcserver # yes
+        $CustomObject | Add-Member -Name "Host" -MemberType NoteProperty -value (get-vmhost -vm $vm).name # yes
+        $CustomObject | Add-Member -Name "HostVersion" -MemberType NoteProperty -value (get-vmhost -vm $vm).version # yes
+        $CustomObject | Add-Member -Name "HostBuild" -MemberType NoteProperty -value (get-vmhost -vm $vm).build # yes
+        $CustomObject | Add-Member -Name "Datacenter" -MemberType NoteProperty -value (Get-Datacenter -Server $vcserver -vm $vm.name) # yes
+        $CustomObject | Add-Member -Name "Cluster" -MemberType NoteProperty -value (Get-Cluster -Server $vcserver -vm $vm.name) # yes
+        $CustomObject | Add-Member -Name "ResourcePool" -MemberType NoteProperty -value (Get-ResourcePool -Server $vcserver -VM $VM) # yes
+        $CustomObject | Add-Member -Name "MemoryGB" -MemberType NoteProperty -value $vm.memorygb # yes
+        $CustomObject | Add-Member -Name "CPU" -MemberType NoteProperty -value $vm.NumCpu # yes
+        $CustomObject | Add-Member -Name "ToolsVersion" -MemberType NoteProperty -value $vm.extensiondata.guest.toolsversion # yes
+        $CustomObject | Add-Member -Name "Notes" -MemberType NoteProperty -value $vm.notes # check
+        $CustomObject | Add-Member -Name "Powerstate" -MemberType NoteProperty -value $vm.powerstate # yes
+        $CustomObject | Add-Member -Name "GuestOS" -MemberType NoteProperty -value $vm.extensiondata.guest.guestfullname # yes
+        $CustomObject | Add-Member -Name "Snapshot" -MemberType NoteProperty -value ($vm | get-snapshot).created # yes
+        $output += $CustomObject
+
+        # -- multiples
         # Loop through each disk and add it to the object.
         $HardDisks = get-HardDisk -VM $VM
         foreach ($HardDisk in $HardDisks) {
@@ -114,7 +144,7 @@ import-module -Name vmware.powercli
 import-Module -Name ImportExcel
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 
-$output = ".\output.xlsx"
+$output = "\\gbcp-isilon100.emea.wdpr.disney.com\eiss\Richard\vCenterExport\export.xlsx"
 
 # delete old file
 remove-item $output -force -ErrorAction SilentlyContinue
@@ -126,17 +156,49 @@ $VC1 = Connect-VIServer -Server "su-gbcp-vvcsa02.emea.wdpr.disney.com" -Credenti
 $VC2 = Connect-VIServer -Server "su-gbcp-vvcsa03.emea.wdpr.disney.com" -Credential $credential
 $VC3 = Connect-VIServer -Server "su-gbcp-vvcsa04.emea.wdpr.disney.com" -Credential $credential
 
-$VirtualMachines = get-VM -server $VC1
-$VirtualMachines += get-VM -server $VC2
-$VirtualMachines += get-VM -server $VC3
-# | where-object NumCpu -ge 18
+$VirtualMachines = get-VM -server $VC1 | where-object NumCpu -ge 18
+#$VirtualMachines += get-VM -server $VC2
+#$VirtualMachines += get-VM -server $VC3
 
 $count = 0
+
+# create an empty XLSX doc with all the headings - not particularly happy with the way I've done this.  I'm sure there is a better way.
+# maybe we don't need to create the NULLs in the function?
+$CustomObject = New-Object -TypeName PSObject
+$CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "AttributeKey" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "AttributeName" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "AttributeValue" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "Tag" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "DiskName" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "DiskLayout" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "DiskDatastore" -MemberType NoteProperty -value $null
+#$CustomObject | Add-Member -Name "DiskDatastoreEncryptionStatus" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "DiskSizeGB" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "NetworkAdapter" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "vCenter" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "Host" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "HostVersion" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "HostBuild" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "Datacenter" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "Cluster" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "ResourcePool" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "MemoryGB" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "CPU" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "ToolsVersion" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "Notes" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "Powerstate" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "GuestOS" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "Snapshot" -MemberType NoteProperty -value $null
+$CustomObject | export-excel $output -append -freezetoprow -autofilter -autosize
 
 foreach ($VirtualMachine in $VirtualMachines){
     $completed = [math]::Round((($count/$VirtualMachines.count) * 100), 2)
 
-    get-VMMetadata -VMName $VirtualMachine | export-excel $output -append -freezetoprow -autofilter -autosize
+    get-VMMetaData -VMName $VirtualMachine.Name | export-excel $output -append -freezetoprow -autofilter -autosize
+    get-VMCoreData -VMName $VirtualMachine.Name | export-excel $output -append -freezetoprow -autofilter -autosize
+
+    
     Write-Progress -Activity "Scan Progress" -Status "$completed% completed." -PercentComplete $completed
     $count += 1
 }
