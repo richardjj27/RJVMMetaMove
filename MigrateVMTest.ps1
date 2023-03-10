ff6ba5948de65fc9960bced899d43ba9d3d04b9b
richardjj27 <60007803+richardjj27@users.noreply.github.com>
2023-03-10 13:47:08 +0000

    Started on the put-VMMetaData function.

--- MigrateVMTest.ps1
+++ MigrateVMTest.ps1
@@ -1,67 +1,23 @@
-# Script to test the export-vmmmetadata function by exporting all VMs.
+# Script to test the migrate-xvc function.
+# initially, do all the hard work here, later, put it all in a function with 3 parameters (VM, sourcevc, destvc)
 
 import-Module -Name vmware.powercli
-import-Module -Name ImportExcel
 import-Module .\export-vmmetadata.psm1
 
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 
-$output = "\\gbcp-isilon100.emea.wdpr.disney.com\eiss\Richard\vCenterExport\vCenterExport.xlsx"
-
-# delete old file
-remove-item $output -force -ErrorAction SilentlyContinue
-
-# Connect to the vCenter Server
 $credential = Get-Credential
-$VC1 = Connect-VIServer -Server "su-gbcp-vvcsa02.emea.wdpr.disney.com" -Credential $credential
-$VC2 = Connect-VIServer -Server "su-gbcp-vvcsa03.emea.wdpr.disney.com" -Credential $credential
-$VC3 = Connect-VIServer -Server "su-gbcp-vvcsa04.emea.wdpr.disney.com" -Credential $credential
+$SourceVC = Connect-VIServer -Server "su-gbcp-vvcsa02.emea.wdpr.disney.com" -Credential $credential
+$DestinationVC = Connect-VIServer -Server "su-gbcp-vvcsa03.emea.wdpr.disney.com" -Credential $credential
+$VMtoMove = "TestVM"
 
-$VirtualMachines = get-VM -server $VC1 | where-object NumCpu -ge 18
-#$VirtualMachines += get-VM -server $VC2
-#$VirtualMachines += get-VM -server $VC3
+$VMMetaDataItems = get-VMMetaData -VMName $VMtoMove
 
-# create an empty XLSX document with all the headings
-$CustomObject = New-Object -TypeName PSObject
-$CustomObject | Add-Member -Name "VMName" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "VMCreated" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "VMVersion" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "AttributeKey" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "AttributeName" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "AttributeValue" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "Tag" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "DiskName" -MemberType NoteProperty -value $null
-#$CustomObject | Add-Member -Name "DiskEncryptionStatus" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "DiskLayout" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "DiskSizeGB" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "DiskDatastore" -MemberType NoteProperty -value $null
-#$CustomObject | Add-Member -Name "DiskDatastoreEncryptionStatus" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "NetworkAdapter" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "vCenter" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "Host" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "HostVersion" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "HostBuild" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "Datacenter" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "Cluster" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "ResourcePool" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "MemoryGB" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "CPU" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "ToolsVersion" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "Notes" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "Powerstate" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "GuestOS" -MemberType NoteProperty -value $null
-$CustomObject | Add-Member -Name "Snapshot" -MemberType NoteProperty -value $null
-$CustomObject | export-excel $output -append -freezetoprow -autofilter -autosize
-
-$count = 0
-foreach ($VirtualMachine in $VirtualMachines){
-    $completed = [math]::Round((($count/$VirtualMachines.count) * 100), 2)
-    get-VMMetaData -VMName $VirtualMachine.Name | export-excel $output -append -freezetoprow -autofilter -autosize
-    get-VMCoreData -VMName $VirtualMachine.Name | export-excel $output -append -freezetoprow -autofilter -autosize
-    Write-Progress -Activity "Scan Progress:" -Status "$completed% completed." -PercentComplete $completed
-    $count++
+# This section below will become the 'put-VMMetaData' function.
+foreach ($VMMetaDataItem in $VMMetaDataItems){
+    write-host $VMMetaDataItem |fl
 }
 
-Disconnect-VIServer -Server $VC1 -Confirm:$false
-Disconnect-VIServer -Server $VC2 -Confirm:$false
-Disconnect-VIServer -Server $VC3 -Confirm:$false
+
+Disconnect-VIServer -Server $SourceVC -Confirm:$false
+Disconnect-VIServer -Server $DestinationVC -Confirm:$false
