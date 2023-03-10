423484c0625c378b6cb9783a3fe5644d3ba8aae7
richardjj27 <60007803+richardjj27@users.noreply.github.com>
2023-03-10 07:19:57 +0000

    Added created dated field

--- export-vmmetadata.ps1
+++ export-vmmetadata.ps1
@@ -39,7 +39,7 @@ function Get-VMMetaData {
         foreach ($attribute in $CustomAttributes) {
             if ($attribute.Value){
                 $CustomObject = New-Object -TypeName PSObject
-                $CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name
+                $CustomObject | Add-Member -Name "VMName" -MemberType NoteProperty -value $VM.name
                 $CustomObject | Add-Member -Name "AttributeKey" -MemberType NoteProperty -value $attribute.key
                 $CustomObject | Add-Member -Name "AttributeName" -MemberType NoteProperty -value ($CustomAttrList |where-object{ $_.Key -eq $Attribute.Key}).Name
                 $CustomObject | Add-Member -Name "AttributeValue" -MemberType NoteProperty -value $attribute.value
@@ -51,7 +51,7 @@ function Get-VMMetaData {
         $CustomTags = Get-TagAssignment -Entity $VM
         foreach ($Tag in $CustomTags) {
             $CustomObject = New-Object -TypeName PSObject
-            $CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name
+            $CustomObject | Add-Member -Name "VMName" -MemberType NoteProperty -value $VM.name
             $CustomObject | Add-Member -Name "Tag" -MemberType NoteProperty -value $Tag.Tag
             $output += $CustomObject
         }
@@ -90,7 +90,8 @@ function Get-VMCoreData {
         #$CustomObject | Add-Member -Name "NetworkAdapter" -MemberType NoteProperty -value $null
 
         # --singles
-        $CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name # yes
+        $CustomObject | Add-Member -Name "VMName" -MemberType NoteProperty -value $VM.name # yes
+        $CustomObject | Add-Member -Name "Created" -MemberType NoteProperty -value $VM.extensiondata.config.createdate # yes
         $CustomObject | Add-Member -Name "vCenter" -MemberType NoteProperty -value $vcserver # yes
         $CustomObject | Add-Member -Name "Host" -MemberType NoteProperty -value (get-vmhost -vm $vm).name # yes
         $CustomObject | Add-Member -Name "HostVersion" -MemberType NoteProperty -value (get-vmhost -vm $vm).version # yes
@@ -113,7 +114,7 @@ function Get-VMCoreData {
         foreach ($HardDisk in $HardDisks) {
             $Datastore = ($HardDisk.Filename | select-string '(?<=\[)[^]]+(?=\])').matches.value 
             $CustomObject = New-Object -TypeName PSObject
-            $CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name
+            $CustomObject | Add-Member -Name "VMName" -MemberType NoteProperty -value $VM.name
             $CustomObject | Add-Member -Name "DiskName" -MemberType NoteProperty -value $HardDisk.Name
             #$CustomObject | Add-Member -Name "DiskEncryptionStatus" -MemberType NoteProperty -value $null
             $CustomObject | Add-Member -Name "DiskLayout" -MemberType NoteProperty -value "S:$($HardDisk.StorageFormat) / P:$($HardDisk.Persistence) / T:$($HardDisk.DiskType)"
@@ -127,7 +128,7 @@ function Get-VMCoreData {
         $NetworkAdapters = Get-NetworkAdapter -VM $VM
         foreach ($NetworkAdapter in $NetworkAdapters){
             $CustomObject = New-Object -TypeName PSObject
-            $CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $VM.name
+            $CustomObject | Add-Member -Name "VMName" -MemberType NoteProperty -value $VM.name
             $CustomObject | Add-Member -Name "NetworkAdapter" -MemberType NoteProperty -value $NetworkAdapter.NetworkName
             $output += $CustomObject
         }
@@ -154,13 +155,14 @@ $VC1 = Connect-VIServer -Server "su-gbcp-vvcsa02.emea.wdpr.disney.com" -Credenti
 $VC2 = Connect-VIServer -Server "su-gbcp-vvcsa03.emea.wdpr.disney.com" -Credential $credential
 $VC3 = Connect-VIServer -Server "su-gbcp-vvcsa04.emea.wdpr.disney.com" -Credential $credential
 
-$VirtualMachines = get-VM -server $VC1 # | where-object NumCpu -ge 18
-$VirtualMachines += get-VM -server $VC2
-$VirtualMachines += get-VM -server $VC3
+$VirtualMachines = get-VM -server $VC1 | where-object NumCpu -ge 18
+#$VirtualMachines += get-VM -server $VC2
+#$VirtualMachines += get-VM -server $VC3
 
 # create an empty XLSX document with all the headings
 $CustomObject = New-Object -TypeName PSObject
-$CustomObject | Add-Member -Name "Server" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "VMName" -MemberType NoteProperty -value $null
+$CustomObject | Add-Member -Name "Created" -MemberType NoteProperty -value $null
 $CustomObject | Add-Member -Name "AttributeKey" -MemberType NoteProperty -value $null
 $CustomObject | Add-Member -Name "AttributeName" -MemberType NoteProperty -value $null
 $CustomObject | Add-Member -Name "AttributeValue" -MemberType NoteProperty -value $null
