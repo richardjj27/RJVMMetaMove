85f83ce700d9085b3a907c1647c9a4e141f4b0a9
Richard Johnson <richardjj627@gmail.com>
2023-03-28 19:17:12 +0100

    Updated todos

--- RJVMMetaMove.psm1
+++ RJVMMetaMove.psm1
@@ -1,16 +1,9 @@
 # Todo:
-# Create a 'Migrate' function
-#   1. Export (Get-VMMetadata) tags/attributes
-#   2. Move
-#   3. Import (Set-VMMetadata) tags/attributes
-#   4. Check and give update
-# Give all the functions and 'RJ' prefix to make them unique.
-# Encryption/disk policy (basically, if datastore is encrypted OR the specific VM is encrypted, its 'yes')
-# This needs to be renamed to cover the generic purpose of the 3 modules here (get-vmmetadata, get-vmcoredata, set-vmmetadata).
 # Create module manifest (.psd1)
 # Learn how to keep function parameters private (or not) and whether to pass an object or text is the right thing to do.
 # Try to find a way of getting some kind of CPU compatibility information.
 # Need to tidy up variable names and follow some kind of convention.
+# Add some error checking.
 
 function Get-RJVMMetaData {
     <#
