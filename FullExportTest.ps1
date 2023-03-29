29396d1b2add27072546dc7fe6c4e8b6c415652a
Richard Johnson <richardjj627@gmail.com>
2023-03-29 11:06:25 +0100

    Added LTAM and APAC

--- FullExportTest.ps1
+++ FullExportTest.ps1
@@ -12,10 +12,16 @@ $credential = Get-Credential
 $VC1 = Connect-VIServer -Server "su-gbcp-vvcsa02.emea.wdpr.disney.com" -Credential $credential
 $VC2 = Connect-VIServer -Server "su-gbcp-vvcsa03.emea.wdpr.disney.com" -Credential $credential
 $VC3 = Connect-VIServer -Server "su-gbcp-vvcsa04.emea.wdpr.disney.com" -Credential $credential
+$VC4 = Connect-VIServer -Server "su-cnts-vcsa01.apac.wdpr.disney.com" -Credential $credential
+$VC5 = Connect-VIServer -Server "su-cnts-vvcsa02.apac.wdpr.disney.com" -Credential $credential
+$VC6 = Connect-VIServer -Server "su-arba-vc01.ltam.wdpr.disney.com" -Credential $credential
 
 $VirtualMachines = get-VM -server $VC1
 $VirtualMachines += get-VM -server $VC2
 $VirtualMachines += get-VM -server $VC3
+$VirtualMachines += get-VM -server $VC4
+$VirtualMachines += get-VM -server $VC5
+$VirtualMachines += get-VM -server $VC6
 
 $count = 0
 foreach ($VirtualMachine in $VirtualMachines){
