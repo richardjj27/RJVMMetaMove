a4c0ac29e85f13956f34888f3c97038854092a03
Richard Johnson <richardjj627@gmail.com>
2023-03-30 13:13:06 +0100

    Configured for global.

--- ExportHostsTest.ps1
+++ ExportHostsTest.ps1
@@ -8,7 +8,7 @@ import-Module .\RJVMMetaMove.psm1
 # P = Just one vCenter (for testing)
 # E = Just Europe
 # G = Global
-$runtype = "P"
+$runtype = "G"
 
 $output = "\\gbcp-isilon100.emea.wdpr.disney.com\eiss\Richard\vCenterExport\vmHostExport [$runtype] $(get-date -Format "yyyy-MM-dd_HH.mm").xlsx"
 
